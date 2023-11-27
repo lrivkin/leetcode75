4e4f184fe93a9a41912769c440ba9e1f606f4471
Leah Rivkin <lrivkin96@gmail.com>
2023-11-27 14:02:08 -0600

    write my own binary search

--- binary-search/2300/pairs.go
+++ binary-search/2300/pairs.go
@@ -6,42 +6,23 @@ import (
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
 	slices.Sort(potions)
-	//fmt.Printf("%v\n", potions)
-
-	// precalculate the number that will be successful + store in a map
-	potionsCountMap := map[int]int{}
-	for i, potion := range potions {
-		if _, seen := potionsCountMap[potion]; seen {
-			continue
-		}
-		potionsCountMap[potion] = len(potions) - i
-	}
-
-	//fmt.Printf("%v\n", potionsCountMap)
+	numPotions := len(potions)
 
 	for i, spell := range spells {
-		searchPotion := int(success) / spell
-		if int(success)%spell != 0 {
-			searchPotion += 1
+
+		left := 0
+		right := numPotions
+
+		for left < right {
+			mid := left + (right-left)/2
+			currValue := int64(potions[mid] * spell)
+			if currValue >= success {
+				right = mid
+			} else {
+				left = mid + 1
+			}
 		}
-
-		//fmt.Printf("success = %d, spell=%d, need minimum potion = %d\n", success, spell, searchPotion)
-		idx, exists := slices.BinarySearch(potions, searchPotion)
-
-		if exists {
-			//fmt.Printf("found minimum potion = %d idx=%d, matches %d spells\n", potions[idx], idx, potionsCountMap[potions[idx]])
-			spells[i] = potionsCountMap[potions[idx]]
-			continue
-		}
-
-		if idx >= len(potions) {
-			//fmt.Printf("no matches\n")
-			spells[i] = 0
-			continue
-		}
-
-		//fmt.Printf("closest match = %d idx=%d, matches %d spells\n", potions[idx], idx, potionsCountMap[potions[idx]])
-		spells[i] = potionsCountMap[potions[idx]]
+		spells[i] = numPotions - left
 	}
 	return spells
 }
