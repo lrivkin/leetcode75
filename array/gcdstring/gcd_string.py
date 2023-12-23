e92f26895bd72ca32ea0506c76b315fb1d5f7a1e
Leah Rivkin <lrivkin96@gmail.com>
2023-12-23 11:27:49 -0700

    solution using numpy gcd

--- array/gcdstring/gcd_string.py
+++ array/gcdstring/gcd_string.py
@@ -1,33 +1,12 @@
-import string
-
+import numpy as np
 
 class Solution:
-
-    def divides_string(self, str1: str, base: str) -> bool:
-        # print(f"divides_string({str1}, {base})")
-        if len(str1)%len(base) != 0:
-            return False
-        
-        repeats = int(len(str1)/len(base))
-        # print(f"k= {repeats}, base*k = {base * repeats}, does divide? {base * repeats == str1}")
-
-        return base * repeats == str1
-
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        base = str1
-        if len(str2) < len(str1):
-            base = str2
+        if str1 + str2 != str2 + str1:
+            return ""
+        gcd = np.gcd(len(str1), len(str2))
 
-
-        while base != "":
-            # print(f"base: {base}")
-            if self.divides_string(str1, base) and self.divides_string(str2, base):
-                return base
-
-            base = base[0:len(base)-1]
-
-        return ""
-
+        return str1[0:gcd]
 
 if __name__ == '__main__':
     s = Solution()
